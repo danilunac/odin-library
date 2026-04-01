a5d67493f1cbe82c043624073e8c5c05991e7dfa
danilunac <182788341+danilunac@users.noreply.github.com>
2026-04-01 17:37:08 +0200

    feat(library): add method to create and store Book instances

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const myLibrary = [];
+const myLibrary = [
+    Book {
+        id: 1,
+        author: 'Gabriel García Márquez',
+        title: 'Cien años de soledad',
+        genre: 'Novela',
+        pages: 700,
+        read: true
+    },
+    Book {
+        id: 2,
+        author: 'Pedro Salinas',
+        title: 'La voz a ti debida',
+        genre: 'Poesía',
+        pages: 200,
+        read: true
+    }
+];
 
 function Book(id, author, title, genre, pages, read = false) {
     if (!new.target) {
@@ -16,4 +33,15 @@ function Book(id, author, title, genre, pages, read = false) {
 // console.log(book1);
 // const book2 = new Book(2, "Pao", "CS50", "Computer Science", 1200);
 // console.log(book2);
+
+function addBookToLibrary(author, title, genre, pages) {
+    const id = crypto.randomUUID();
+    const book = new Book(id, author, title, genre, pages);
+    myLibrary.push(book);
+}
+
+addBookToLibrary("Stefan Zweig", "El mundo de ayer", "Biografía", 890);
+addBookToLibrary("George Orwell", "1984", "Novela", 400);
+console.log(myLibrary);
+
 
